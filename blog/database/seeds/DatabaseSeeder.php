e9ec76727d6273b5d76f5b0dee0a0a7cee858bc3
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-27 22:43:48 +0200

    creata la relazione

--- blog/database/seeds/DatabaseSeeder.php
+++ blog/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            BlogSeeder::class
+            BlogSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
