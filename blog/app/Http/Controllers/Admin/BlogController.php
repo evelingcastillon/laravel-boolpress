393f72b89b22a9175bb9c00aadbe325a312680a5
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-28 17:21:33 +0200

    controller category

--- blog/app/Http/Controllers/Admin/BlogController.php
+++ blog/app/Http/Controllers/Admin/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Blog;
+use App\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -27,7 +28,9 @@ class BlogController extends Controller
      */
     public function create()
     {
-        return view('admin.blogs.create');
+
+        $categories = Category::all();
+        return view('admin.blogs.create', compact('categories'));
     }
 
     /**
